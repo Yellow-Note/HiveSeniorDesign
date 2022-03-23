56299410b10eae57051714fd14887bbacd908d22
sriggs2 <43488703+sriggs2@users.noreply.github.com>
2022-03-22 18:27:56 -0600

    add stand in image

---
+++ vueapp/hive2/src/components/GoogleMap.vue
@@ -0,0 +1,24 @@
+<template>
+  <GoogleMap
+    api-key="API-KEY!!!"
+    style="width: 100%; height: 500px"
+    :center="center"
+    :zoom="15"
+    >
+      <Marker :options="{ position: center }" />
+    </GoogleMap>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { GoogleMap, Marker } from 'vue3-google-map'
+
+export default defineComponent({
+  components: { GoogleMap, Marker },
+  setup() {
+    const center = { lat: 41.1544, lng: -105.3745 }
+
+    return { center }
+  },
+})
+</script>
--- vueapp/hive2/src/views/GPSView.vue
+++ vueapp/hive2/src/views/GPSView.vue
@@ -4,31 +4,43 @@
             <div class="row">
                 <div class="col-8">
                     <div class="map-responsive">
-                        <iframe style="position: absolute;"
-                            height = "60%"
-                            width= "100%"
-                            loading="lazy"
-                            allowfullscreen
-                            src="https://www.google.com/maps/embed/v1/place?key=!!!API-KEY!!!
-                                    &q=University+of+Wyoming,Laramie+WY">
-                        </iframe>
+                        <GoogleMap class="map-responsive-googlemap"></GoogleMap>
                     </div>
                 </div>
                 <div class="col-4">
-                    <div class="list-group" >
-                        <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                    <div class="list-group">
+                        <button type="button" class="btn btn-primary">Manual Location</button>
+                        <div>
+                        <a href="#" class="list-group-item list-group-item-action" aria-current="true">
                             First Pin
                         </a>
                         <a href="#" class="list-group-item list-group-item-action">Second pin</a>
                         <a href="#" class="list-group-item list-group-item-action">Third Pin</a>
                         <a href="#" class="list-group-item list-group-item-action">Fourth Pin</a>
                         <a class="list-group-item list-group-item-action disabled">disabled Pin</a>
+                        </div>
+                    <img src="../assets/logo.png" alt="nope">
                     </div>
                 </div>
             </div>
         </div>    
     </div>
 </template>
+<script>
+import GoogleMap from '../components/GoogleMap.vue'
+export default {
+    components: {
+        GoogleMap
+    }, data() {
+        return {
+            
+        }
+    },
+}
+
+
+</script>
+
 
 <style scope>
 .map-responsive{
@@ -37,7 +49,7 @@
     position:relative;
     height:0;
 }
-.map-responsive iframe{
+.map-responsive-googlemap{
     left:5%; 
     top:0;
     height:100%;
