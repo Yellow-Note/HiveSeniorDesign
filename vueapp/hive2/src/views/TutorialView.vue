4c0900ea58ca785249b533dcab99f0b4c639e419
sriggs2 <43488703+sriggs2@users.noreply.github.com>
2022-03-23 17:46:12 -0600

    Co-authored-by: Andrew Fuller <afulle12@users.noreply.github.com>

--- vueapp/hive2/src/views/TutorialView.vue
+++ vueapp/hive2/src/views/TutorialView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="Tutorial">
-    <h1>This is our Tutorial page</h1>
+    <h1>This is our tutorial page!!!</h1>
+    <h1>This is a test of github copilot</h1>
   </div>
 </template>
