b8c9044d7b225ab54e8958cada8ef96360102bae
sriggs2 <43488703+sriggs2@users.noreply.github.com>
2022-03-23 22:50:59 -0600

    Co-authored-by: Andrew Fuller <afulle12@users.noreply.github.com> Co-authored-by: cjlarsen1165@gmail.com <cjlarsen1165@gmail.com>

--- vueapp/hive2/src/views/HomeView.vue
+++ vueapp/hive2/src/views/HomeView.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Testing</h1>
+    <img class="img-fluid" :src="image" alt="Side Bar Picture">
+
+    <div class="mb-3">
+      <label for="formFile" class="form-label">Import .MOV File</label>
+      <input class="form-control" type="file" @change="importFile($event)">
+      <div class="form-text">Select one ".MOV" file to import at a time.</div>
+    </div>
+    <div class="d-grid gap-2">
+      <button class="btn btn-primary" type="button" @click="changeImage" >Change Image</button>
+    </div>
+    <!--<file-selector v-model="files">
+      <dropzone v-slot
+    </file-selector> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+const { ipcRenderer } = require('electron')
+window.ipcRenderer = ipcRenderer
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      images: ["@/assets/logo.png", "@/assets/images.png"],
+      image: require("@/assets/logo.png")
+    }
+  },
+  methods: {
+    importfile(event){
+      if(event.target.files.length > 0) {
+        var filePath = event.target.files[0].path
+        /* eslint-disable no-unused-vars */
+        window.ipcRenderer.on('importfile', (event, images) => {
+          this.images = images
+
+        })
+        ipcRenderer.send('importfile', filePath)
+      }
+    },
+    changeImage() {
+      this.image = require(this.images[1])
+    }
   }
 }
 </script>
